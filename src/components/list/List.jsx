036ca4bed36cd97fd0550ef3ddf753b0346552ba
aritra <chatterjee.aritra@protonmail.com>
2023-03-14 01:53:29 +0530

    hover card

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -4,15 +4,19 @@ import ListItem from "../listItem/ListItem";
 import "./list.scss";
 
 const List = () =>{
-  const [slideNumber, setSlideNumber] = useState(0)
+  const [isMoved, setIsMoved] = useState(false);
+  const [slideNumber, setSlideNumber] = useState(0);
   const listRef = useRef()
   const handleClick = (direction) =>{
+    setIsMoved(true)
     let distance = listRef.current.getBoundingClientRect().x - 50
-    if(direction === "left"){
+    if(direction === "left" && slideNumber > 0){
+      setSlideNumber(slideNumber - 1)
       listRef.current.style.transform = `translateX(${230 + distance}px)`
     }
     
-    if(direction === "right"){
+    if(direction === "right" && slideNumber < 5){
+      setSlideNumber(slideNumber + 1)
       listRef.current.style.transform = `translateX(${-230 + distance}px)`
     }
   }
@@ -20,14 +24,9 @@ const List = () =>{
     <div className="list">
       <span className="listTitle">Continue to watch</span>
       <div className="wrapper">
-        <ArrowBackIosOutlined className="sliderArrow left" onClick={()=>handleClick("left")}/>
+        <ArrowBackIosOutlined className="sliderArrow left" onClick={()=>handleClick("left")}
+         style={{display: !isMoved && "none"}}/>
         <div className="container" ref={listRef}>
-          <ListItem/> 
-          <ListItem/> 
-          <ListItem/> 
-          <ListItem/> 
-          <ListItem/> 
-          <ListItem/> 
           <ListItem/> 
           <ListItem/> 
           <ListItem/> 
