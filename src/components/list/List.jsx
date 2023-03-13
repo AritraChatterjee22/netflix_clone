a7ebcb6000fc90bc2880ba2051fbe2b3e147f6a5
aritra <chatterjee.aritra@protonmail.com>
2023-03-13 17:05:19 +0530

    lists

---
+++ src/components/list/List.jsx
@@ -0,0 +1,48 @@
+import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from "@mui/icons-material";
+import { useRef, useState } from "react";
+import ListItem from "../listItem/ListItem";
+import "./list.scss";
+
+const List = () =>{
+  const [slideNumber, setSlideNumber] = useState(0)
+  const listRef = useRef()
+  const handleClick = (direction) =>{
+    let distance = listRef.current.getBoundingClientRect().x - 50
+    if(direction === "left" && slideNumber>0){
+      listRef.current.style.transform = `translateX(${230 + distance}px)`
+    }
+    
+    if(direction === "right"){
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`
+    }
+  }
+  return(
+    <div className="list">
+      <span className="listTitle">Continue to watch</span>
+      <div className="wrapper">
+        <ArrowBackIosOutlined className="sliderArrow left" onClick={()=>handleClick("left")}/>
+        <div className="container" ref={listRef}>
+          <ListItem/> 
+          <ListItem/> 
+          <ListItem/> 
+          <ListItem/> 
+          <ListItem/> 
+          <ListItem/> 
+          <ListItem/> 
+          <ListItem/> 
+          <ListItem/> 
+          <ListItem/> 
+          <ListItem/> 
+          <ListItem/> 
+          <ListItem/> 
+          <ListItem/> 
+          <ListItem/> 
+          <ListItem/> 
+        </div>
+        <ArrowForwardIosOutlined className="sliderArrow right" onClick={()=>handleClick("right")}/>
+      </div>
+    </div>
+  )
+}
+
+export default List
