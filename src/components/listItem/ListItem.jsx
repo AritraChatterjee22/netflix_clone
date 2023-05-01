dcd64f9b2477cd97260e92400dbc045ff3d3e1de
aritra <chatterjee.aritra@protonmail.com>
2023-05-01 21:51:19 +0530

    List and ListItem commit

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,9 +1,14 @@
 import { Add, PlayArrow, ThumbDownOutlined, ThumbUpAltOutlined } from "@mui/icons-material";
+import { useState } from "react";
 import "./listItem.scss";
 
-const ListItem = () =>{
+const ListItem = ({index}) =>{
+  const [isHovered, setIsHovered] = useState(false);
   return(
-    <div className="listItem">
+    <div className="listItem"
+    style={{left: isHovered && index * 225 - 50 + index * 2.5}}
+    onMouseEnter={()=>setIsHovered(true)}
+    onMouseLeave={()=>setIsHovered(false)}>
       <img src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee" alt=""/> 
       <div className="itemInfo">
         <div className="icons">
